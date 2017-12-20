e35804aa9336f913800b802a700bbc264f6aac0d
deepseafishing <onejunekang@gmail.com>
2017-12-19 21:56:16 -0500

    setting up firebase

--- fire/setup.js
+++ fire/setup.js
@@ -1,1 +1,13 @@
-throw new Error('fire/setup.js does not contain a config. You need to run `npm prepare`')
+export default firebase =>
+  // Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+  // You will also need to load the Firebase SDK.
+  // See https://firebase.google.com/docs/web/setup for more details.
+
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAfdwZlI8O6yH8vQou-qZ2EX99JpqoOMMM',
+    databaseURL: 'https://togobox-ab248.firebaseio.com',
+    storageBucket: 'togobox-ab248.appspot.com',
+    authDomain: 'togobox-ab248.firebaseapp.com',
+    messagingSenderId: '596575621684',
+    projectId: 'togobox-ab248'
+  });
