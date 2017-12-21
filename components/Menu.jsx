2c723d7f3540258ff085f7d7dbd4d2f926c2bce0
Alvin Wen <alvinwen424@gmail.com>
2017-12-20 23:04:37 -0500

    initial menu component

---
+++ components/Menu.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Menu extends Component {
+  render () {
+    return (
+      <div >
+        <div>
+          <h3>filter</h3>
+        </div>
+        <div>
+          <h3>menu</h3>
+        </div>
+        <div>
+          <h3>map</h3>
+        </div>
+      </div>
+    )
+  }
+}
--- components/Routes.jsx
+++ components/Routes.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 
+import Menu from './Menu'
 import App from '../App'
 
 export default class Routes extends Component {
   render () {
     return (
       <Switch>
+        <Routes path="/menu" component={Menu} />
         <Route path="/" component={App} />
       </Switch>
     )
