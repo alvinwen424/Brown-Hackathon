d5fcf1d94d5da63f69eef01b09fb093a6f674fb0
Stanley Tiu <stanleytiu7@gmail.com>
2017-12-20 23:05:15 -0500

    starting point of branch

--- components/Routes.jsx
+++ components/Routes.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import App from '../App'
+import App from '~/App'
+import hello from './hello'
 
 export default class Routes extends Component {
-  render () {
+  render() {
     return (
       <Switch>
+        <Route path="/hello" component={hello} />
         <Route path="/" component={App} />
       </Switch>
     )
