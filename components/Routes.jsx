aa92cbcc056ac40306322e2381d6771ea984040e
deepseafishing <onejunekang@gmail.com>
2018-01-03 00:24:54 -0500

    resolve: warning message

--- components/Routes.jsx
+++ components/Routes.jsx
@@ -4,17 +4,18 @@ import history from '../history';
 import hello from './hello';
 import Menu from './Menu';
 import Home from './Home';
-import SignUpContainer from './SignUpContainer';
-import LoginContainer from './LoginContainer';
+import SignUp from './SignUp';
+import Login from './Login';
 
 export default class Routes extends Component {
   render() {
     return (
       <Router history={history}>
         <Switch>
-          <Route path="/menu" component={Menu} />
-          <Route path="/login" component={LoginContainer} />
-<Route path="/signup" component={SignUpContainer} />;          <Route path="/" component={Home} />
+          <Route path="/menu" render={() => <Menu />} />
+          <Route path="/login" render={() => <Login />} />
+          <Route path="/signup" render={() => <SignUp />} />
+          <Route path="/" render={() => <Home />} />
         </Switch>
       </Router>
     );
