f71c620986003d5ab326abda40c3ddf2dfc4064f
Alvin Wen <alvinwen424@gmail.com>
2018-01-27 11:14:00 -0500

    api keys updated

--- fire/setup.js
+++ fire/setup.js
@@ -4,11 +4,19 @@ export default firebase =>
 // See https://firebase.google.com/docs/web/setup for more details.
 
 firebase.initializeApp({
-  "apiKey": "AIzaSyAfdwZlI8O6yH8vQou-qZ2EX99JpqoOMMM",
-  "databaseURL": "https://togobox-ab248.firebaseio.com",
-  "storageBucket": "togobox-ab248.appspot.com",
-  "authDomain": "togobox-ab248.firebaseapp.com",
-  "messagingSenderId": "596575621684",
-  "projectId": "togobox-ab248"
+    apiKey: "AIzaSyADdpnrE8oeTRmC_F2ZQbb4QD4V1q0cQbw",
+    authDomain: "brown-hackathon.firebaseapp.com",
+    databaseURL: "https://brown-hackathon.firebaseio.com",
+    projectId: "brown-hackathon",
+    storageBucket: "brown-hackathon.appspot.com",
+    messagingSenderId: "943416784152"
 });
 
+
+
+  // "apiKey": "AIzaSyAfdwZlI8O6yH8vQou-qZ2EX99JpqoOMMM",
+  // "databaseURL": "https://togobox-ab248.firebaseio.com",
+  // "storageBucket": "togobox-ab248.appspot.com",
+  // "authDomain": "togobox-ab248.firebaseapp.com",
+  // "messagingSenderId": "596575621684",
+  // "projectId": "togobox-ab248"
