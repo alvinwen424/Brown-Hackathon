2a33531725020dcaddfc1b31682a0bfa1e9c54b2
Alvin Wen <alvinwen424@gmail.com>
2018-01-27 15:45:19 -0500

    working query for housing

--- components/Housing.jsx
+++ components/Housing.jsx
@@ -5,19 +5,39 @@ export default class Housing extends Component {
   constructor(){
     super()
     this.state = {
-      houses: {}
+      houses: []
     }
   }
 
   componentDidMount() {
-    let houses = db.collection('housing').doc("yN3TNjJJs4mP4rk4fGis")
-    console.log('housing,', houses)
+   db.collection('housing').get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        this.setState({houses: [...this.state.houses, doc.data()]})
+        console.log('doc', doc.data(), doc.id)
+      })
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    })
   }
 
   render(){
+    let { houses } = this.state
+    console.log('homes', houses)
     return(
       <div>
         <h1>housing</h1>
+        { houses.length && houses.map((home) => {
+          return (
+            <div>
+              <h3>{home.Street} {home.City} {home.State}</h3>
+              <h4>Price: {home.Price}</h4>
+              <h4>Bedrooms: {home.Rooms} </h4>
+              <h4>Restrooms: {home.Restroom}</h4>
+            </div>
+          )
+        })}
       </div>
     )
   }
