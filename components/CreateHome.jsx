e1cceb19baade7f148adaeebcd3f44215422368e
Alvin Wen <alvinwen424@gmail.com>
2018-01-27 22:53:17 -0500

    can upload multiple images

--- components/CreateHome.jsx
+++ components/CreateHome.jsx
@@ -5,28 +5,43 @@ export default class CreateHome extends Component {
   constructor() {
     super()
     this.state ={
-      file: {}
+      file: []
     }
   }
 
   onChange = (e) => {
-    this.setState({file: e.target.files[0]})
+    this.setState({file: [...this.state.file, e.target.files[0]]})
     console.log('file', e.target.files[0] )
   }
 
   onSubmit = (e) => {
     let { file } = this.state
-    e.preventDefault()
+    let { email } = auth.currentUser
     let storageRef = storage.ref()
-    storageRef.child(`/images/${file.name}`).put(file).then((snapshot) => {
-       console.log('uploaded a file')
-    })
+    e.preventDefault()
+    storageRef.constructor.prototype.putFiles = function(files) {
+      return Promise.all(files.map(function(file) {
+        return storageRef.child(`/images/${email}/${file.name}`).put(file)
+      }));
+    }
+
+    storageRef.putFiles(file).then(function(metadatas) {
+      // Get an array of file metadata
+      console.log('metaData', metadatas)
+    }).catch(function(error) {
+      // If any task fails, handle this
+    });
+    // let storageRef = storage.ref()
+    // storageRef.child(`/images/${email}/${file.name}`).put(file).then((snapshot) => {
+    //    console.log('uploaded a file')
+    // })
   }
 
   render (){
     return(
       <div>
         <form onSubmit={this.onSubmit}>
+
           <input type="file" onChange={this.onChange} />
           <button type="submit">Upload</button>
         </form>
