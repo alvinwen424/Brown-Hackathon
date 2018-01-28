075e4f8a3e450c5f5693b7a4e40e0493b2f90e28
deepseafishing <onejunekang@gmail.com>
2018-01-28 00:14:51 -0500

    style: housing

--- history.js
+++ history.js
@@ -1,7 +1,7 @@
 // Imported from node_modules/history folder using static routes
-import createHistory from 'history/createBrowserHistory'
+import createHistory from 'history/createBrowserHistory';
 
-const history = createHistory()
+const history = createHistory();
 
 // Export history object. To use history.push method, just import 'history' to any file you want
-export default history
+export default history;
